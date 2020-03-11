cd3782a81f05dff8f0ec73c05571ae72a47d03db
h-otter <h-otter@outlook.jp>
2020-03-11 21:03:39 +0900

    add response serializer

--- app/bbs/serializer.py
+++ app/bbs/serializer.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from bbs.models import Thread
+from bbs.models import Thread, Response
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = ('display_name', 'comment')
+        read_only_fields = ('responded_by', 'responded_at')
 
 
 class ThreadSerializer(serializers.ModelSerializer):
@@ -8,6 +15,8 @@ class ThreadSerializer(serializers.ModelSerializer):
     responses_count = serializers.IntegerField(read_only=True)
     read_responses_count = serializers.IntegerField(read_only=True)
     last_responded_at = serializers.DateTimeField(read_only=True)
+
+    # response = ResponseSerializer(write_only=True)
 
     class Meta:
         model = Thread
